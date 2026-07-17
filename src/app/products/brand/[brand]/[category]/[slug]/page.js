8473555a862dccad2a3ee380c477bb9bfe12faff
Ith Chanakijkamjorn <ithcha2525@gmail.com>
2026-07-17 12:41:33 +0700

    Delete old [category] route to fix ambiguous route error

--- src/app/products/brand/[brand]/[category]/[slug]/page.js
+++
@@ -1,2 +0,0 @@
-export { default } from '../../[mainCategory]/[subCategory]/[slug]/page.js';
-export { generateStaticParams } from '../../[mainCategory]/[subCategory]/[slug]/page.js';
--- src/app/products/brand/[brand]/[category]/page.js
+++
@@ -1,2 +0,0 @@
-export { default } from '../[mainCategory]/page.js';
-export { generateStaticParams } from '../[mainCategory]/page.js';
